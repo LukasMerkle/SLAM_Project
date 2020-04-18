b0e458feb760453454fe7d3ff77fa66b847d95e7
lmerkle <lmerkle@andrew.cmu.edu>
2020-04-18 18:23:51 -0400

    cleaned up code

--- scripts/plane_extraction.py
+++ scripts/plane_extraction.py
@@ -36,11 +36,11 @@ class PlaneExtracter():
         return plane_pointcloud_all
         # o3d.visualization.draw_geometries(pointcloud_list)
 
-    def visualize_poindclouds(self,pcd1,pcd2):
-        pcd1.paint_uniform_color([1,0,0])
-        pcd2.paint_uniform_color([0,1,0])
-        all_points = [pcd2,pcd1]
-        o3d.visualization.draw_geometries(all_points)
+def visualize_poindclouds(pcd1,pcd2):
+    pcd1.paint_uniform_color([1,0,0])
+    pcd2.paint_uniform_color([0,1,0])
+    all_points = [pcd2,pcd1]
+    o3d.visualization.draw_geometries(all_points)
 
 def convert(x_s, y_s, z_s):
     scaling = 0.005 # 5 mm
@@ -76,55 +76,67 @@ def load_velodyne_timestep(f_bin):
     hits = np.asarray(hits)
     return hits
 
-def main(args):
-    #good frame 1335705713193323
-    feature_extractor = PlaneExtracter()
+def sort_paths(directory):
+    #sorts files with increasing timestamps
     file_name_list = []
-    for f in os.listdir(sys.argv[1]):
+    for f in os.listdir(directory):
         file_name_list.append(int(f[:-4]))
     sorted_path_list = sorted(file_name_list)
+    return sorted_path_list
+
+def create_open3d_pointcloud(cloud):
+    #creates open3d pointcloud from numpy pointcloud
+    cloud_open3d= o3d.geometry.PointCloud()
+    cloud_open3d.points = o3d.utility.Vector3dVector(cloud)
+    return cloud_open3d
+
+def main(args):
+    #good frame 1335705713193323
 
     if len(sys.argv) is 3:
         start_at_desired_frame = True
         desired_time_stamp = int(sys.argv[2])
         print("starting at desired time stamp\n")
     else: start_at_desired_frame = False
-    counter = 0
+    
     planes_all = []
     transformation = np.eye(4)
     plane_extractor = PlaneExtracter()
+    
+    #Get start frame for ICP evaluation
     first_bin = open(sys.argv[1]+str(desired_time_stamp)+".bin", "rb")
-    first_cloud = cloud = load_velodyne_timestep(first_bin)
+    first_cloud = load_velodyne_timestep(first_bin)
     first_cloud_open3d= o3d.geometry.PointCloud()
     first_cloud_open3d.points = o3d.utility.Vector3dVector(first_cloud[:,:3])
-    first_cloud_open3d.estimate_normals()
-    # first_cloud_plane = plane_extractor.extract_planes(first_cloud)
-    # first_cloud_open3d.points = o3d.utility.Vector3dVector(first_cloud_plane[:,:3])
+    recent_clouds = []
 
-    for i,f in enumerate(sorted_path_list):
+    #Loop through all lidar scans and start with desired scan
+    for i,f in enumerate(sort_paths(sys.argv[1])):
+        #check if current scan is the desired start scan
         if start_at_desired_frame is True and f-desired_time_stamp < 0:
             continue
         start_at_desired_frame = False
         f_bin = open(sys.argv[1]+str(f)+".bin", "rb")
         cloud = load_velodyne_timestep(f_bin)
-        planes_all.append(cloud[:,:3])
-        if len(planes_all) == 2:
-            cloud_open3d_1= o3d.geometry.PointCloud()
-            cloud_open3d_1.points = o3d.utility.Vector3dVector(planes_all[0])
-            cloud_open3d_2= o3d.geometry.PointCloud()
-            cloud_open3d_2.points = o3d.utility.Vector3dVector(planes_all[1])
+        recent_clouds.append(cloud[:,:3])
 
-            reg_p2l = o3d.registration.registration_icp(cloud_open3d_1, cloud_open3d_2, 2, np.eye(4),o3d.registration.TransformationEstimationPointToPoint(),o3d.registration.ICPConvergenceCriteria(max_iteration = 200))
-            evaluation = o3d.registration.evaluate_registration(cloud_open3d_1, cloud_open3d_2,2, reg_p2l.transformation)
-            transformation = np.array(reg_p2l.transformation) @ transformation
-            del planes_all[0]
-            
-            #TEST AFTER N SECONDS
-            if f-desired_time_stamp >  2*10**6:
-                print(np.linalg.inv(transformation))
-                transformed_point_cloud = copy.deepcopy(first_cloud_open3d)
-                transformed_point_cloud.transform(transformation)
-                plane_extractor.visualize_poindclouds(transformed_point_cloud,cloud_open3d_2)
+        #Skip ICP if for the first frame
+        if len(recent_clouds) is not 2:
+            continue
+
+        cloud_open3d_1=create_open3d_pointcloud(recent_clouds[0])
+        cloud_open3d_2=create_open3d_pointcloud(recent_clouds[1])
+
+        reg_icp = o3d.registration.registration_icp(cloud_open3d_1, cloud_open3d_2, 2, np.eye(4),o3d.registration.TransformationEstimationPointToPoint(),o3d.registration.ICPConvergenceCriteria(max_iteration = 200))
+        transformation = np.array(reg_icp.transformation) @ transformation
+        del recent_clouds[0]
+        
+        #TEST AFTER N SECONDS
+        if f-desired_time_stamp >  2*10**6:
+            print(np.linalg.inv(transformation))
+            transformed_point_cloud = copy.deepcopy(first_cloud_open3d)
+            transformed_point_cloud.transform(transformation)
+            visualize_poindclouds(transformed_point_cloud,cloud_open3d_2)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
